fc1f8f9ec8a9613a1f2cae68dbbfc0e0f4ef78c3
nscarbrough <nscarbrough@MUSHROOM>
2025-05-15 09:27:42 -0600

    DiceGameFinished

--- DiceGame/DiceGame.cpp
+++ DiceGame/DiceGame.cpp
@@ -1,23 +1,56 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
 using namespace std;
 
-int main() {
-    int number;
-    cout << "Enter a number: ";
-    cin >> number;
-    cout << "You entered: " << number << endl;
-    return 0;
+int rollDie() {
+    return rand() % 6 + 1;
 }
 
-
-using namespace std;
-
 int main() {
-    srand(time(0)); // Seed random number generator
-    int randomNumber = rand() % 6 + 1; // Random number from 1 to 6
-    cout << "Random number: " << randomNumber << endl;
+
+    int score1 = 0, score2 = 0;
+
+    srand(time(0));
+
+
+    cout << "\n--- Dice Rolling Game ---\n";
+
+    for (int round = 1; round <= 3; round++) {
+        cout << "\nRound " << round << ":\n";
+
+        int roll1 = rollDie() + rollDie();
+        int roll2 = rollDie() + rollDie();
+
+        cout << "player 1" << " rolls a total of : " << roll1 << endl;
+        cout << "player 2" << " rolls a total of: " << roll2 << endl;
+
+        if (roll1 > roll2) {
+            score1++;
+            cout << "player 1" << " wins this round!\n";
+        }
+        else if (roll2 > roll1) {
+            score2++;
+            cout << "player 2" << " wins this round!\n";
+        }
+        else {
+            cout << "It's a tie this round!\n";
+        }
+    }
+
+    cout << "\n--- Final Results ---\n";
+    cout << "player 1" << "'s score: " << score1 << endl;
+    cout << "player 2" << "'s score: " << score2 << endl;
+
+    if (score1 > score2) {
+        cout << "Winner: " << "player 1" << "!\n";
+    }
+    else if (score2 > score1) {
+        cout << "Winner: " << "player 2" << "!\n";
+    }
+    else {
+        cout << "It's a tie!\n";
+    }
+
     return 0;
 }
